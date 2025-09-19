c7b35e1729dc00a4a715ab8a2b1d7c2464a33a0f
meruyert2025 <m_abdigaliyeva@kbtu.kz>
2025-09-19 13:45:16 -0700

    My first commit

---
+++ Lab1/14.py
@@ -0,0 +1,16 @@
+a = "Hello, World!"
+print(a.upper())
+
+
+
+b = " Hello, World! "
+print(b.strip()) # returns "Hello, World!"
+
+
+c = "Hello, World!"
+print(c.replace("H", "J"))
+
+
+
+d = "Hello, World!"
+print(d.split(",")) # returns ['Hello', ' World!']
